4043e927909385c992eb1fab03f577ed1236d762
Duy Phan <phanleduy131120@gmail.com>
2022-12-02 10:49:04 +0700

    .

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const appRoute = require('./src/routes');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.use(cors());
+var app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', appRoute);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
